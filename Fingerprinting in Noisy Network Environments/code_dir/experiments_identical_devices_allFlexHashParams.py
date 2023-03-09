903d58ae679b7484d9aef7fb0c0a1cd90a4e7cd0
Nathan Thom <nathanthom@nevada.unr.edu>
2023-03-09 13:07:08 -0800

    Remove individual experiment files and updates allFlexHash params experiment to accept device name

--- Fingerprinting in Noisy Network Environments/code_dir/experiments_identical_devices_allFlexHashParams.py
+++ Fingerprinting in Noisy Network Environments/code_dir/experiments_identical_devices_allFlexHashParams.py
@@ -26,128 +26,152 @@ path_to_same_cam_uncleaned_no_interaction = "/home/nthom/Documents/SmartRecon/Fi
 
 path_to_simhash = "/home/nthom/Documents/SmartRecon/Fingerprinting in Noisy Network Environments/data/simhashes/"
 
-c_uc_list = [1]
-device_list = [1, 2, 3]
+device_list = int(input("Select one of the following: \n1. Plug \n2. Light \n3. Cam \n4. All\n"))
+if not device_list in [1, 2, 3, 4]:
+    raise ValueError(
+        "'device_list' selection must be one of the following values: 1 or 2,"
+    )
+if device_list == 1:
+    device_list = [1]
+elif device_list == 2:
+    device_list = [2]
+elif device_list == 3:
+    device_list = [3]
+else:
+    device_list = [1, 2, 3]
+
 accum_list = [128, 256, 512, 1024]
 window_list = [4, 5, 6]
 
-for c_uc in c_uc_list:
-    if c_uc == 1:
-        c_uc = "cleaned"
-    else:
-        c_uc = "uncleaned"
+all_params_best_model_performance = [["Device", "Accum", "Window", "Combo", "Model Name", "f1_micro", "accuracy",
+                                      "balanced_accuracy", "mcc"]]
+c_uc = "cleaned"
+for device in device_list:
+    if device == 1:
+        device = "plug"
+    elif device == 2:
+        device = "light"
+    elif device == 3:
+        device = "cam"
 
-    for device in device_list:
-        if device == 1:
-            device = "plug"
-        elif device == 2:
-            device = "light"
-        elif device == 3:
-            device = "cam"
+    for accum in accum_list:
+        names = [f"dim{i}" for i in range(accum // 8)]
+        names.append("class")
 
-        for accum in accum_list:
-            names = [f"dim{i}" for i in range(accum // 8)]
-            names.append("class")
+        for window in window_list:
+            if window == 4:
+                combo_list = [2, 3, 4]
+            elif window == 5:
+                combo_list = [2, 3, 4, 5]
+            else:
+                combo_list = [2, 3, 4, 5, 6]
 
-            for window in window_list:
-                if window == 4:
-                    combo_list = [2, 3, 4]
-                elif window == 5:
-                    combo_list = [2, 3, 4, 5]
-                else:
-                    combo_list = [2, 3, 4, 5, 6]
+            for combo in combo_list:
+                csv_list = []
 
-                for combo in combo_list:
-                    csv_list = []
+                target_dir = f"{path_to_simhash}{device}/accum_{accum}/window_{window}/combo_{combo}/{c_uc}/"
+                for j in os.listdir(target_dir):
+                    csv_list.append(target_dir + j)
 
-                    target_dir = f"{path_to_simhash}{device}/accum_{accum}/window_{window}/combo_{combo}/{c_uc}/"
-                    for j in os.listdir(target_dir):
-                        csv_list.append(target_dir + j)
+                name_of_current_data = f"FlexHash-identical{device}AllFlexHashParams-{device}-accum_{accum}-window_" \
+                                       f"{window}-combo_{combo}-cleaned"
+                dataset = combine_csv(csv_list, names)
+                dataset.reset_index(drop=True, inplace=True)
 
-                    name_of_current_data = f"FlexHash-identicalDeviceAllFlexHashParams-{device}-accum_{accum}-window_" \
-                                           f"{window}-combo_{combo}-cleaned"
-                    dataset = combine_csv(csv_list, names)
-                    dataset.reset_index(drop=True, inplace=True)
-
-                    print(f"*** Total samples in {name_of_current_data}: {len(dataset.index)} ***")
-                    for device_name in sorted(dataset["class"].unique()):
-                        num_samples = len((dataset[dataset["class"] == device_name]).index)
-                        print(
-                            f"*** Samples for device: {device_name} in {name_of_current_data}: {num_samples} "
-                            f"({num_samples/dataset.shape[0]}%) ***"
-                        )
-
-                    # x is the entire dataframe except for the class column
-                    x = dataset.drop(["class"], axis=1)
-
-                    # y_original is an unaltered list of all values in the class column
-                    y_original = dataset["class"].values.tolist()
-
-                    # y is a dataframe of only the class column and the values have been converted to numeric
-                    # representation
-                    counter = 0
-                    y_temp = dataset["class"].tolist()
-                    for unique_value in sorted(dataset["class"].unique()):
-                        for index, value in enumerate(dataset["class"]):
-                            if value == unique_value:
-                                y_temp[index] = counter
-                        counter += 1
-                    dataset["class"] = y_temp
-
-                    y = dataset["class"]
-
-                    x_train, x_test, y_train, y_test = train_test_split(
-                        x.values, y.values, test_size=0.2, stratify=y.values
+                print(f"*** Total samples in {name_of_current_data}: {len(dataset.index)} ***")
+                for device_name in sorted(dataset["class"].unique()):
+                    num_samples = len((dataset[dataset["class"] == device_name]).index)
+                    print(
+                        f"*** Samples for device: {device_name} in {name_of_current_data}: {num_samples} "
+                        f"({num_samples/dataset.shape[0]}%) ***"
                     )
 
-                    names = list(range(x_train.shape[1]))
-                    train_dataset_df = pd.DataFrame(x_train, columns=names)
-                    train_dataset_df.insert(train_dataset_df.shape[1], "class", y_train)
+                # x is the entire dataframe except for the class column
+                x = dataset.drop(["class"], axis=1)
 
-                    names = list(range(x_test.shape[1]))
-                    test_dataset_df = pd.DataFrame(x_test, columns=names)
-                    test_dataset_df.insert(test_dataset_df.shape[1], "class", y_test)
+                # y_original is an unaltered list of all values in the class column
+                y_original = dataset["class"].values.tolist()
 
-                    del (
-                        x,
-                        y,
-                        y_original,
-                        x_train,
-                        y_train,
-                        x_test,
-                        y_test,
-                        names,
-                        dataset,
-                        y_temp
-                    )
+                # y is a dataframe of only the class column and the values have been converted to numeric
+                # representation
+                counter = 0
+                y_temp = dataset["class"].tolist()
+                for unique_value in sorted(dataset["class"].unique()):
+                    for index, value in enumerate(dataset["class"]):
+                        if value == unique_value:
+                            y_temp[index] = counter
+                    counter += 1
+                dataset["class"] = y_temp
 
-                    model_save_path = f"agModels-{name_of_current_data}_{gethostname()}"
+                y = dataset["class"]
 
-                    train_dataset_td = TabularDataset(train_dataset_df)
-                    label = "class"
-                    print("Summary of class variable: \n", train_dataset_td[label].describe())
+                x_train, x_test, y_train, y_test = train_test_split(
+                    x.values, y.values, test_size=0.2, stratify=y.values
+                )
 
-                    predictor = TabularPredictor(
-                        eval_metric="f1_micro", label="class", path=model_save_path
-                    ).fit(train_dataset_td, presets="best_quality",
-                          excluded_model_types=["CAT", "KNN", "RF", "FASTAI", "LR", "NN_TORCH", "AG_AUTOMM"],
-                          keep_only_best=True
-                          )
+                col_names = list(range(x_train.shape[1]))
+                train_dataset_df = pd.DataFrame(x_train, columns=col_names)
+                train_dataset_df.insert(train_dataset_df.shape[1], "class", y_train)
 
-                    results = predictor.fit_summary()
+                col_names = list(range(x_test.shape[1]))
+                test_dataset_df = pd.DataFrame(x_test, columns=col_names)
+                test_dataset_df.insert(test_dataset_df.shape[1], "class", y_test)
 
-                    predictor = TabularPredictor.load(model_save_path)
+                del (
+                    x,
+                    y,
+                    y_original,
+                    x_train,
+                    y_train,
+                    x_test,
+                    y_test,
+                    dataset,
+                    y_temp
+                )
 
-                    test_dataset_td = TabularDataset(test_dataset_df)
-                    y_test = test_dataset_td[label]
-                    test_data_noLabel = test_dataset_td.drop(columns=[label])
+                model_save_path = f"agModels-{name_of_current_data}_{gethostname()}"
 
-                    y_pred = predictor.predict(test_data_noLabel)
-                    perf = predictor.evaluate_predictions(
-                        y_true=y_test, y_pred=y_pred, auxiliary_metrics=True
-                    )
+                train_dataset_td = TabularDataset(train_dataset_df)
 
-                    leaderboard_df = predictor.leaderboard(test_dataset_td)
-                    leaderboard_df.to_csv(
-                        f"agLeaderboard_{name_of_current_data}_{gethostname()}.csv"
-                    )
+                # subsample_size = 1600
+                # train_dataset_td = train_dataset_td.sample(n=subsample_size, random_state=0)
+
+                label = "class"
+                print("Summary of class variable: \n", train_dataset_td[label].describe())
+
+                del (train_dataset_df)
+
+                predictor = TabularPredictor(
+                    eval_metric="f1_micro", label="class", path=model_save_path
+                ).fit(train_dataset_td, presets="best_quality",
+                      excluded_model_types=["CAT", "KNN", "RF", "FASTAI", "LR", "NN_TORCH", "AG_AUTOMM"],
+                      keep_only_best=True
+                      )
+
+                results = predictor.fit_summary()
+
+                test_dataset_td = TabularDataset(test_dataset_df)
+
+                # test_dataset_td = test_dataset_td.sample(n=subsample_size, random_state=0)
+
+                del (train_dataset_td, test_dataset_df)
+
+                best_model_name = predictor.get_model_best()
+                best_model_scores = predictor.evaluate(test_dataset_td)
+
+                best_model_output_list = [device, accum, window, combo, best_model_name]
+                for key in best_model_scores.keys():
+                    best_model_output_list.append(best_model_scores[key])
+
+                all_params_best_model_performance.append(best_model_output_list)
+
+                # print(f"\n\n######\n{best_model_output_list}\n#####\n")
+
+                predictor.save_space()
+
+            # break
+        # break
+    # break
+
+output_df = pd.DataFrame(all_params_best_model_performance)
+output_df.to_csv(f"{device}_allFlexHashParams_{gethostname()}.csv", index=False)
